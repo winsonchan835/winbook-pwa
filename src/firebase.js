e8d9fc0349dca41332022ef13e131a383c3c06b0
winsonchan835 <chanw8352gmail.com>
2021-04-16 00:46:35 -0400

    added firebase config and baseline react app

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCFDdwZxlQn588cE2HTkNFnorIsz0P7Cfs",
+  authDomain: "winbook-pwa.firebaseapp.com",
+  projectId: "winbook-pwa",
+  storageBucket: "winbook-pwa.appspot.com",
+  messagingSenderId: "993484211899",
+  appId: "1:993484211899:web:9fa42f43153d3a979e0b9e",
+  measurementId: "G-YQ82DJ8FKK"
+};
