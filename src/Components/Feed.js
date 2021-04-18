bb144cec0a8710cd6e570d792fdd1a3ddb454a6d
winsonchan835 <chanw8352gmail.com>
2021-04-18 16:19:23 -0400

    feature: posts

--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -9,7 +9,21 @@ function Feed() {
     <div className='feed'>
       <StoryReel />
       <MessageSender />
-      <Post />
+      <Post
+      key='1'
+      profilePic='https://image.flaticon.com/icons/png/128/3033/3033143.png'
+      message='hello there'
+      timestamp='this is a timestamp'
+      username='chanw'
+      image='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfyNtCM2SBewRilvmRiBDM5uPsXV7jb6KeBw&usqp=CAUU'
+      />
+      <Post
+      key='2'
+      profilePic='https://image.flaticon.com/icons/png/128/3033/3033143.png'
+      message='hello there'
+      timestamp='this is a timestamp'
+      username='chanw'
+      />
     </div>
   )
 }
--- src/Components/Post.js
+++ src/Components/Post.js
@@ -1,9 +1,46 @@
+import { Avatar } from '@material-ui/core';
 import React from 'react';
 import './Post.css';
 
-function Post() {
+import ThumbUpIcon from '@material-ui/icons/ThumbUp';
+import ChatBubbleIcon from '@material-ui/icons/ChatBubble';
+import ShareIcon from '@material-ui/icons/Share';
+
+function Post({ profilePic, image, username, timestamp, message }) {
   return (
-    <div>
+    <div className='post'>
+      <div className='post-top'>
+        <Avatar className='post-avatar' src={profilePic}/>
+        <div className='post-top-info'>
+          <h3>{username}</h3>
+          <p>{timestamp}</p>
+        </div>
+      </div>
+
+      <div className='post-bottom'>
+        <p>{message}</p>
+      </div>
+
+      <div className='post-image'>
+        <img src={image} alt='' />
+      </div>
+
+      <div className='post-options'>
+        <div className='post-option'>
+          <ThumbUpIcon />
+          <p>Like</p>
+        </div>
+        <div className='post-option'>
+          <ChatBubbleIcon />
+          <p>Comment</p>
+        </div>
+        <div className='post-option'>
+          <ShareIcon />
+          <p>Share</p>
+        </div>
+
+
+      </div>
 
     </div>
   )
